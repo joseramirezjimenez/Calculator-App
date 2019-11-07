ed6e8d2e31e98a81962a71fccbd109b9f1da15cf
joseramirezjimenez <jose.ramirez5019b@gmail.com>
2019-11-07 12:15:08 -0600

    Added in Calculator omponent and styled the App and Calculator components

---
+++ src/components/Calculator/Calculator.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Calculator.css';
+import Screen from '../Screen/Screen';
+
+/* Main calculator component. Contains our other calculator components */
+class Calculator extends Component {
+    render() {
+        return (
+            <div className="Calculator">
+                <Screen />
+            </div>
+        );
+    }
+}
+
+export default Calculator;
